4cc5cfe4b9a510ccf5f41e60bcc7f6a9a18dbeef
Manish <manishlokhande96@gmail.com>
2019-11-11 15:35:20 -0800

    First commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    googleClientID : '794710347574-nvlt0qnce1uu3l8520ms5q59carb4o0v.apps.googleusercontent.com',
+    googleClientSecret : 'XC5cCsezmZVk6akRrxmDD0TP',
+    faceBookID : '420512042200052',
+    faceBookSecret : '90a1be431b1d8042b18d53fdef4cc282',
+    AwsAccessKeyId : 'AKIAIP4X3P4455WW3K2Q',
+    AwsSecretAccessKey : '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',
+    region : 'us-east-2',
+    bucketName : 'manish-dropbox',
+    AwsS3Url:  'https://manish-dropbox.s3.us-east-2.amazonaws.com',
+    AwsDynamoDbUrl: 'https://dynamodb.us-east-2.amazonaws.com',
+    MongoURI: 'mongodb+srv://manishDb:ty2TOCRijaDwJhUY@emaily-2r7gc.mongodb.net/test?retryWrites=true&w=majority'
+};
