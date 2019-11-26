b6d839f4cd68c59094bc41e0fb1a132e02ccef7e
Manish <manishlokhande96@gmail.com>
2019-11-26 11:32:07 -0800

    notification  related UI added

--- routes/schedule.js
+++ routes/schedule.js
@@ -59,6 +59,7 @@ router.get('/add',ensureAuthenticated,(req,res)=>{
             
             user: currentuser,
             data: data,
+            editFlag:false,
             logins: {}
         })
     })
@@ -66,11 +67,11 @@ router.get('/add',ensureAuthenticated,(req,res)=>{
 
 router.post('/add', (req,res)=>{
 
-    const { pName, tablet, startDate, endDate, morningCnt, middayCnt, eveningCnt, bedtimeCnt, docName} = req.body;
+    const { sName,pName, tablet, startDate, endDate, morningCnt, middayCnt, eveningCnt, bedtimeCnt, docName, confirmBtn, deleteBtn} = req.body;
 
     let errors = [];
 
-    if(!pName || !tablet || !startDate ||!endDate || !docName){
+    if(!sName || !pName || !tablet || !startDate ||!endDate || !docName){
         errors.push({ msg:'Please fill all required fields!' });
     }
     
@@ -87,43 +88,113 @@ router.post('/add', (req,res)=>{
     else{
 
         const dynamoDbObj = require('./../models/connect');
-        var myScheduleName = 'mySchedule'+('-')+Date.now();
 
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth() + 1; //January is 0!
+        if(deleteBtn == 'delete'){
+            //for delete schedule
+            console.log('Inside delete');
+            var params = {
+                TableName: 'files',
+                Key: {
+                    'fileName': sName
+                }
+            };
+            
+            dynamoDbObj.delete(params, function (err, data) {
+        
+                if (err) {
+                    console.log(err);
+                    req.flash('error_msg','Error Occured while deleting schedule!');
+                    res.redirect('/schedule/add');
+                } else {
+                    console.log('Schedule deleted');
+                    req.flash('success_msg','Schedule Deleted!');
+                    res.redirect('/schedule/view');
+                }
+            });
 
-        var yyyy = today.getFullYear();
-        if (dd < 10) {
-        dd = '0' + dd;
-        } 
-        if (mm < 10) {
-        mm = '0' + mm;
-        } 
-        var today = dd + '/' + mm + '/' + yyyy;
+        }
+        else{
+            //for add new and confirm schedule
+            var myScheduleName,myScheduleDesc;
+            
+            if(!(sName.includes('mySchedule'))){
+                myScheduleName = 'mySchedule'+('-')+Date.now();
+                myScheduleDesc=sName;
+                
+                var today = new Date();
+                var dd = today.getDate();
+                var mm = today.getMonth() + 1; //January is 0!
+        
+                var yyyy = today.getFullYear();
+                if (dd < 10) {
+                dd = '0' + dd;
+                } 
+                if (mm < 10) {
+                mm = '0' + mm;
+                } 
+                var today = dd + '/' + mm + '/' + yyyy;
 
-        var input = {
-            'name': req.user.name,'fileName': myScheduleName, 'startDate': startDate, 'endDate': endDate, 'morningTabCnt': morningCnt,'expiryDate':endDate, 'modifiedDate':today,
-            'middayTabCnt': middayCnt, 'eveTabCnt': eveningCnt, 'bedtimeTabCnt': bedtimeCnt , 'tabletName' : tablet, 'patientName':pName, 'docName':docName, 'createdDate':today
-        };
-
-        var paramsDb = {
-            TableName: 'files',
-            Item:  input
-        };
-
-        dynamoDbObj.put(paramsDb, function (err, data) {
-                    
-            if (err) {
-                console.log(err);
-                req.flash('error_msg','Error Occured while creating schedule!');
-                res.redirect('/schedule/add');
-            } else {
-                console.log('Schedule Added');
-                req.flash('success_msg','Schedule Created!');
-                res.redirect('/schedule/view');
             }
-        });
+            else{
+                myScheduleName=sName;
+            }
+
+            var input = {
+                'name': req.user.name,'fileName': myScheduleName, 'fileDesc':myScheduleDesc, 'startDate': startDate, 'endDate': endDate, 'morningTabCnt': morningCnt,'expiryDate':endDate, 'modifiedDate':today,
+                'middayTabCnt': middayCnt, 'eveTabCnt': eveningCnt, 'bedtimeTabCnt': bedtimeCnt , 'tabletName' : tablet, 'patientName':pName, 'docName':docName, 'createdDate':today
+            };
+
+            var paramsDb = {
+                TableName: 'files',
+                Item:  input
+            };
+
+            dynamoDbObj.put(paramsDb, function (err, data) {
+                        
+                if (err) {
+                    console.log(err);
+                    req.flash('error_msg','Error Occured while creating schedule!');
+                    res.redirect('/schedule/add');
+                } else {
+                    console.log('Schedule Added');
+                    req.flash('success_msg','Schedule Created!');
+                    res.redirect('/schedule/view');
+                }
+            });
+        }  
+    }
+});
+
+router.post('/edit', (req,res)=>{
+
+    const { fileName, user, editFlag, editBtn, activateBtn} = req.body;
+
+    if(editBtn == 'edit'){
+
+        var params= {
+            TableName: 'files',
+            Key: {
+                'fileName': fileName
+            }
+        };
+    
+        dynamoDbObj.get(params, function (err, data) {
+    
+            if (err){ throw err}
+            else{
+                res.render('addSchedule',{
+                    user: user,
+                    data: data.Item,
+                    editFlag: editFlag,
+                    logins: { }
+                })
+            }
+        })
+
+    }
+    else if(activateBtn == 'activate'){
+
+        res.render('activate');
     }
 });
 
